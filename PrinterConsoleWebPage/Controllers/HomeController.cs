76af3092bccf3db604cf30af7557a47162b80064
Starling Germosen <sgrysoft@outlook.com>
2019-01-02 07:58:41 -0400

    webpage creation

---
+++ PrinterConsoleWebPage/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PrinterConsoleWebPage.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult PrintInvoiceThermal(string[] arrs)
+        {
+            var p = new Process();
+
+            // Get the file path of your Application (exe)
+            string filePath = @"C:\PraySoft\PrinterConsoleApp.exe";
+            // string[] arr1 = new string[] { "one", "two", "three" };
+            // dd[0] = "ssss";
+            var info = new ProcessStartInfo(filePath, String.Join(" ", arrs));
+            info.RedirectStandardOutput = true;
+            info.UseShellExecute = false;
+
+            p = Process.Start(info);
+            return View();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+         
+    }
+}
